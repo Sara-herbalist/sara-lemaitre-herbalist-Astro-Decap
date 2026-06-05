26cd80abe32ae98522a216a985bbb37aab666f86
Sara-herbalist <sara.lemaitre.herbalist@hotmail.com>
2026-06-05 17:28:19 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  site: 'https://www.saralemaitre.com',import { defineConfig } from 'astro/config';
-export default defineConfig({
   site: 'https://www.saralemaitre.com',
-  output: 'static',
-  build: { format: 'directory' },
-  trailingSlash: 'ignore'
-});
-
   output: 'static',
   build: { format: 'directory' },
   trailingSlash: 'ignore'
